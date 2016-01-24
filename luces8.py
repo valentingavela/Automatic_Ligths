42a1bdd66693aa016d5134ff1e87880d8da70ce7
valentinsabbath <valentingavela@gmail.com>
2016-01-24 00:07:42 -0300

    Update luces8.py
    Le faltan unos acomodos

--- luces8.py
+++ luces8.py
@@ -4,37 +4,53 @@ import datetime
 GPIO = webiopi.GPIO
 
 LIGHT18 = 18 # GPIO pin using BCM numbering
+
 HOUR_ON_18  = 8  # Turn Light ON at 08:00
+MINUTE_ON_18 = 25
 HOUR_OFF_18 = 18 # Turn Light OFF at 18:00
+MINUTE_OFF_18 = 25
 
 LIGHT23 = 23 # GPIO pin using BCM numbering
 HOUR_ON_23  = 8  # Turn Light ON at 08:00
+MINUTE_ON_23 =
 HOUR_OFF_23 = 18 # Turn Light OFF at 18:00
+MINUTE_OFF_23 =
 
 LIGHT24 = 24 # GPIO pin using BCM numbering
 HOUR_ON_24  = 8  # Turn Light ON at 08:00
+MINUTE_ON_24 =
 HOUR_OFF_24 = 18 # Turn Light OFF at 18:00
+MINUTE_OFF_24 =
 
 LIGHT25 = 25 # GPIO pin using BCM numbering
 HOUR_ON_25  = 8  # Turn Light ON at 08:00
+MINUTE_ON_25=
 HOUR_OFF_25 = 18 # Turn Light OFF at 18:00
+MINUTE_OFF_25=
 
 LIGHT8 = 8 # GPIO pin using BCM numbering
 HOUR_ON_8  = 8  # Turn Light ON at 08:00
+MINUTE_ON_8 =
 HOUR_OFF_8 = 18 # Turn Light OFF at 18:00
+MINUTE_OFF_8 =
 
 LIGHT7 = 7 # GPIO pin using BCM numbering
 HOUR_ON_7  = 8  # Turn Light ON at 08:00
+MINUTE_ON_7 = 
 HOUR_OFF_7 = 18 # Turn Light OFF at 18:00
+MINUTE_OFF_7 =
 
 LIGHT20 = 20 # GPIO pin using BCM numbering
 HOUR_ON_20  = 8  # Turn Light ON at 08:00
+MINUTE_ON_20 =
 HOUR_OFF_20 = 18 # Turn Light OFF at 18:00
+MINUTE_OFF_20 =
 
 LIGHT21 = 21 # GPIO pin using BCM numbering
 HOUR_ON_21  = 8  # Turn Light ON at 08:00
+MINUTE_ON_21 =
 HOUR_OFF_21 = 18 # Turn Light OFF at 18:00
-
+MINUTE_OFF_21 =
 
 # setup function is automatically called at WebIOPi startup
 def setup():
@@ -82,82 +98,82 @@ def loop():
     now = datetime.datetime.now()
 
     # toggle light ON all days at the correct time
-    if ((now.hour == HOUR_ON_18) and (now.minute == 0) and (now.second == 0)):
-        if (GPIO.digitalRead(LIGHT18) == GPIO.LOW):
-            GPIO.digitalWrite(LIGHT18, GPIO.HIGH)
-
-    if ((now.hour == HOUR_ON_23) and (now.minute == 0) and (now.second == 0)):
-    	if (GPIO.digitalRead(LIGHT23) == GPIO.LOW):
-            GPIO.digitalWrite(LIGHT23, GPIO.HIGH)
-
-    if ((now.hour == HOUR_ON_24) and (now.minute == 0) and (now.second == 0)):
-        if (GPIO.digitalRead(LIGHT24) == GPIO.LOW):
-            GPIO.digitalWrite(LIGHT24, GPIO.HIGH)
-
-    if ((now.hour == HOUR_ON_25) and (now.minute == 0) and (now.second == 0)):
-    	if (GPIO.digitalRead(LIGHT25) == GPIO.LOW):
-            GPIO.digitalWrite(LIGHT25, GPIO.HIGH)
-
-    if ((now.hour == HOUR_ON_8) and (now.minute == 0) and (now.second == 0)):
-        if (GPIO.digitalRead(LIGHT8) == GPIO.LOW):
-            GPIO.digitalWrite(LIGHT8, GPIO.HIGH)
-
-    if ((now.hour == HOUR_ON_7) and (now.minute == 0) and (now.second == 0)):
-    	if (GPIO.digitalRead(LIGHT7) == GPIO.LOW):
-            GPIO.digitalWrite(LIGHT7, GPIO.HIGH)
-
-    if ((now.hour == HOUR_ON_20) and (now.minute == 0) and (now.second == 0)):
-        if (GPIO.digitalRead(LIGHT20) == GPIO.LOW):
-            GPIO.digitalWrite(LIGHT20, GPIO.HIGH)
-
-    if ((now.hour == HOUR_ON_21) and (now.minute == 0) and (now.second == 0)):
-    	if (GPIO.digitalRead(LIGHT21) == GPIO.LOW):
-            GPIO.digitalWrite(LIGHT21, GPIO.HIGH)
-    
-	# toggle light OFF
-    if ((now.hour == HOUR_OFF_18) and (now.minute == 0) and (now.second == 0)):
+    if ((now.hour == HOUR_ON_18) and (now.minute == MINUTE_ON_18)): #and (now.second == 0)):
         if (GPIO.digitalRead(LIGHT18) == GPIO.HIGH):
             GPIO.digitalWrite(LIGHT18, GPIO.LOW)
 
-    if ((now.hour == HOUR_OFF_23) and (now.minute == 0) and (now.second == 0)):
-        if (GPIO.digitalRead(LIGHT23) == GPIO.HIGH):
+    if ((now.hour == HOUR_ON_23) and (now.minute == MINUTE_ON_23)): #and (now.second == 0)):
+    	if (GPIO.digitalRead(LIGHT23) == GPIO.HIGH):
             GPIO.digitalWrite(LIGHT23, GPIO.LOW)
-	
-    if ((now.hour == HOUR_OFF_24) and (now.minute == 0) and (now.second == 0)):
+
+    if ((now.hour == HOUR_ON_24) and (now.minute == MINUTE_ON_24)): # and (now.second == 0)):
         if (GPIO.digitalRead(LIGHT24) == GPIO.HIGH):
             GPIO.digitalWrite(LIGHT24, GPIO.LOW)
 
-    if ((now.hour == HOUR_OFF_25) and (now.minute == 0) and (now.second == 0)):
-        if (GPIO.digitalRead(LIGHT25) == GPIO.HIGH):
+    if ((now.hour == HOUR_ON_25) and (now.minute == MINUTE_ON_25) #and (now.second == 0)):
+    	if (GPIO.digitalRead(LIGHT25) == GPIO.HIGH):
             GPIO.digitalWrite(LIGHT25, GPIO.LOW)
-	
-    if ((now.hour == HOUR_OFF_8) and (now.minute == 0) and (now.second == 0)):
+
+    if ((now.hour == HOUR_ON_8) and (now.minute == MINUTE_ON_8)): #and (now.second == 0)):
         if (GPIO.digitalRead(LIGHT8) == GPIO.HIGH):
             GPIO.digitalWrite(LIGHT8, GPIO.LOW)
 
-    if ((now.hour == HOUR_OFF_7) and (now.minute == 0) and (now.second == 0)):
-        if (GPIO.digitalRead(LIGHT7) == GPIO.HIGH):
+    if ((now.hour == HOUR_ON_7) and (now.minute == MINUTE_ON_7)): #and (now.second == 0)):
+    	if (GPIO.digitalRead(LIGHT7) == GPIO.HIGH):
             GPIO.digitalWrite(LIGHT7, GPIO.LOW)
-	
-    if ((now.hour == HOUR_OFF_20) and (now.minute == 0) and (now.second == 0)):
+
+    if ((now.hour == HOUR_ON_20) and (now.minute == MINUTE_ON_20)): #and (now.second == 0)):
         if (GPIO.digitalRead(LIGHT20) == GPIO.HIGH):
             GPIO.digitalWrite(LIGHT20, GPIO.LOW)
 
-    if ((now.hour == HOUR_OFF_21) and (now.minute == 0) and (now.second == 0)):
-        if (GPIO.digitalRead(LIGHT21) == GPIO.HIGH):
+    if ((now.hour == HOUR_ON_21) and (now.minute == MINUTE_ON_21)): #and (now.second == 0)):
+    	if (GPIO.digitalRead(LIGHT21) == GPIO.HIGH):
             GPIO.digitalWrite(LIGHT21, GPIO.LOW)
+    
+	####################### toggle light OFF ##################
+    if ((now.hour == HOUR_OFF_18) and (now.minute == MINUTE_OFF_18): #and (now.second == 0)):
+        if (GPIO.digitalRead(LIGHT18) == GPIO.LOW):
+            GPIO.digitalWrite(LIGHT18, GPIO.HIGH)
+
+    if ((now.hour == HOUR_OFF_23) and (now.minute == MINUTE_OFF_23)): #and (now.second == 0)):
+        if (GPIO.digitalRead(LIGHT23) == GPIO.LOW):
+            GPIO.digitalWrite(LIGHT23, GPIO.HIGH)
+	
+    if ((now.hour == HOUR_OFF_24) and (now.minute == MINUTE_OFF_24)): #and (now.second == 0)):
+        if (GPIO.digitalRead(LIGHT24) == GPIO.LOW):
+            GPIO.digitalWrite(LIGHT24, GPIO.HIGH)
+
+    if ((now.hour == HOUR_OFF_25) and (now.minute == MINUTE_OFF_25)): #and (now.second == 0)):
+        if (GPIO.digitalRead(LIGHT25) == GPIO.LOW):
+            GPIO.digitalWrite(LIGHT25, GPIO.HIGH)
+	
+    if ((now.hour == HOUR_OFF_8) and (now.minute == MINUTE_OFF_8)): #and (now.second == 0)):
+        if (GPIO.digitalRead(LIGHT8) == GPIO.LOW):
+            GPIO.digitalWrite(LIGHT8, GPIO.HIGH)
+
+    if ((now.hour == HOUR_OFF_7) and (now.minute == MINUTE_OFF_7)): #and (now.second == 0)):
+        if (GPIO.digitalRead(LIGHT7) == GPIO.LOW):
+            GPIO.digitalWrite(LIGHT7, GPIO.HIGH)
+	
+    if ((now.hour == HOUR_OFF_20) and (now.minute == MINUTE_OFF_20)): and (now.second == 0)):
+        if (GPIO.digitalRead(LIGHT20) == GPIO.LOW):
+            GPIO.digitalWrite(LIGHT20, GPIO.HIGH)
+
+    if ((now.hour == HOUR_OFF_21) and (now.minute == MINUTE_OFF_21)): #and (now.second == 0)):
+        if (GPIO.digitalRead(LIGHT21) == GPIO.LOW):
+            GPIO.digitalWrite(LIGHT21, GPIO.HIGH)
 
     # gives CPU some time before looping again
     webiopi.sleep(1)
 
 # destroy function is called at WebIOPi shutdown
 def destroy():
-    GPIO.digitalWrite(LIGHT18, GPIO.LOW)
-    GPIO.digitalWrite(LIGHT23, GPIO.LOW)
-    GPIO.digitalWrite(LIGHT24, GPIO.LOW)
-    GPIO.digitalWrite(LIGHT25, GPIO.LOW)
-    GPIO.digitalWrite(LIGHT8, GPIO.LOW)
-    GPIO.digitalWrite(LIGHT7, GPIO.LOW)
-    GPIO.digitalWrite(LIGHT20, GPIO.LOW)
-    GPIO.digitalWrite(LIGHT21, GPIO.LOW)
+    GPIO.digitalWrite(LIGHT18, GPIO.HIGH)
+    GPIO.digitalWrite(LIGHT23, GPIO.HIGH)
+    GPIO.digitalWrite(LIGHT24, GPIO.HIGH)
+    GPIO.digitalWrite(LIGHT25, GPIO.HIGH)
+    GPIO.digitalWrite(LIGHT8, GPIO.HIGH)
+    GPIO.digitalWrite(LIGHT7, GPIO.HIGH)
+    GPIO.digitalWrite(LIGHT20, GPIO.HIGH)
+    GPIO.digitalWrite(LIGHT21, GPIO.HIGH)
 
